89f0d7af136b78850f8493180690a7c704bcf45a
Krzysztof Pilarczyk <radiator0@o2.pl>
2020-10-25 21:01:53 +0100

    Adjusted schema sql

--- CREATE_SCHEMA.sql
+++ CREATE_SCHEMA.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "categories"
 (
     "category_id" NUMBER(5) NOT NULL,
-    "name" VARCHAR2(255),
-    "short_name" VARCHAR2(10),
+    "name" VARCHAR2(255) NOT NULL,
+    "short_name" VARCHAR2(10) NOT NULL,
     "description" VARCHAR2(1000),
     CONSTRAINT "categories_pk" PRIMARY KEY ("category_id")
 );
@@ -10,7 +10,7 @@ CREATE TABLE "categories"
 CREATE TABLE "publishers"
 (
     "publisher_id" NUMBER(5) NOT NULL,
-    "name" VARCHAR2(255),
+    "name" VARCHAR2(255) NOT NULL,
     "year_of_foundation" NUMBER(4),
     "description" VARCHAR2(1000),
     "website" VARCHAR2(255),
@@ -22,8 +22,8 @@ CREATE TABLE "publishers"
 CREATE TABLE "authors"
 (
     "author_id" NUMBER(5) NOT NULL,
-    "names" VARCHAR2(255),
-    "surname" VARCHAR2(255),
+    "names" VARCHAR2(255) NOT NULL,
+    "surname" VARCHAR2(255) NOT NULL,
     "nationality" VARCHAR2(255),
     "nickname" VARCHAR2(255),
     "bio" VARCHAR2(1000),
@@ -33,15 +33,15 @@ CREATE TABLE "authors"
 CREATE TABLE "books"
 (
     "isbn" VARCHAR2(13) NOT NULL,
-    "title" VARCHAR2(255),
+    "title" VARCHAR2(255) NOT NULL,
     "description" VARCHAR2(1000),
     "publication_year" NUMBER(4),
     "number_of_pages" NUMBER(4),
     "hard_cover" NUMBER(1),
     "language" VARCHAR(255),
-    "price" NUMBER(5, 2),
-    "quantity_in_stock" NUMBER(5),
-    "quantity_sold" NUMBER(5),
+    "price" NUMBER(5, 2) NOT NULL,
+    "quantity_in_stock" NUMBER(5) NOT NULL,
+    "quantity_sold" NUMBER(5) NOT NULL,
     "format" VARCHAR2(5),
     "category_id" NUMBER(5) NOT NULL,
     "publisher_id" NUMBER(5) NOT NULL,
@@ -49,12 +49,15 @@ CREATE TABLE "books"
     CONSTRAINT "books_pk" PRIMARY KEY ("isbn"),
     CONSTRAINT "fk_category" FOREIGN KEY ("category_id") REFERENCES "categories"("category_id"),
     CONSTRAINT "fk_publisher" FOREIGN KEY ("publisher_id") REFERENCES "publishers"("publisher_id"),
-    CONSTRAINT "fk_author" FOREIGN KEY ("author_id") REFERENCES "authors"("author_id")
+    CONSTRAINT "fk_author" FOREIGN KEY ("author_id") REFERENCES "authors"("author_id"),
+    CONSTRAINT "price_gt_0" CHECK ("price" > 0),
+    CONSTRAINT "quantity_in_stock_gtq_0" CHECK ("quantity_in_stock" >= 0),
+    CONSTRAINT "quantity_sold_gtq_0" CHECK ("quantity_sold" >= 0)
 );
 
 CREATE TABLE "reviews"
 (
-    "review_id" NUMBER(5) NOT NULL,
+    "review_id" NUMBER(5) NOT NULL, -- dodac auto increment
     "author" VARCHAR2(255),
     "rating" NUMBER(2),
     "content" VARCHAR2(1000),
@@ -70,22 +73,23 @@ CREATE TABLE "reviews"
 CREATE TABLE "customers"
 (
     "email" VARCHAR2(255) NOT NULL,
-    "username" VARCHAR2(255),
-    "password" VARCHAR2(255),
+    "username" VARCHAR2(255) NOT NULL,
+    "password" VARCHAR2(255) NOT NULL,
     "phone_number" VARCHAR2(15),
     "gender" CHAR(1),
     "birth_date" DATE,
-    "registration_date" DATE,
+    "registration_date" DATE NOT NULL,
     CONSTRAINT "customers_pk" PRIMARY KEY ("email")
 );
 
 CREATE TABLE "addresses"
 (
     "address_id" NUMBER(5) NOT NULL,
-    "street" VARCHAR2(255),
-    "postcode" VARCHAR2(5),
-    "city" VARCHAR2(255),
-    "house_number" VARCHAR2(10),
+    "street" VARCHAR2(255) NOT NULL,
+    "postcode" VARCHAR2(6) NOT NULL,
+    "city" VARCHAR2(255) NOT NULL,
+    -- "country" VARCHAR2(255) NOT NULL, pominąć przy ładowaniu
+    "house_number" VARCHAR2(10) NOT NULL,
     "apartment_number" VARCHAR2(10),
     CONSTRAINT "addresses_pk" PRIMARY KEY ("address_id")
 );
