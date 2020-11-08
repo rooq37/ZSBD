9b56f746c4754ab2b6b26bb2bba719029d0256e6
Krzysztof Pilarczyk <radiator0@o2.pl>
2020-11-08 16:44:30 +0100

    Change t1 & t5.

--- transactionsmeter/src/main/resources/transaction-1.sql
+++ transactionsmeter/src/main/resources/transaction-1.sql
@@ -1,3 +1,4 @@
 SELECT t1."email", (SELECT COUNT(*) FROM "orders" s1 WHERE s1."customer_email" = t1."email") ordersCount
 FROM "customers" t1
+WHERE EXTRACT( YEAR from t1."registration_date") > 2011
 ORDER BY 2 DESC
--- transactionsmeter/src/main/resources/transaction-5.sql
+++ transactionsmeter/src/main/resources/transaction-5.sql
@@ -1,1 +1,8 @@
 UPDATE "books" t1 SET "price" = "price" + (SELECT COUNT(*)/10 FROM "books" s1 WHERE t1."author_id" = s1."author_id")
+WHERE (
+    SELECT SUM(s2."quantity") FROM "orders" s1
+    JOIN "order_items" s2 ON s1."order_id" = s2."order_id"
+    WHERE s2."isbn" = t1."isbn"
+    GROUP BY s2."isbn"
+    HAVING COUNT(*) > 2
+) > 5
