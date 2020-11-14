cf13fe0893bce16d7f542b0856cb70911f5cac15
MateuszStarczyk <mati.starczyk@gmail.com>
2020-11-14 18:15:13 +0100

    added 9-th with index

---
+++ transactionsmeter/src/main/resources/transaction-9.sql
@@ -0,0 +1,14 @@
+SELECT COUNT(*) FROM "orders" o
+JOIN "order_items" oi ON o."order_id" = oi."order_id"
+WHERE (
+    SELECT AVG(rate) FROM (  
+        SELECT r."rating" rate, b."isbn" isb
+        FROM "reviews" r, "books" b
+        WHERE  b."isbn" = r."isbn" 
+        AND b."publication_year" IN (2000, 2001)
+    )
+    WHERE isb = oi."isbn"
+    GROUP BY isb
+) > 5
+AND o."status" = 'ORDER_SHIPPED'
+GROUP BY o."order_id"
