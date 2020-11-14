12389e687bff61c442331534514a57cc51a1184f
Krzysztof Pilarczyk <radiator0@o2.pl>
2020-11-14 14:46:15 +0100

    Added city_addresses index.

--- transactionsmeter/src/main/resources/transaction-3.sql
+++ transactionsmeter/src/main/resources/transaction-3.sql
@@ -4,6 +4,5 @@ JOIN "addresses" t2 ON t1."invoice_address_id" = t2."address_id"
 JOIN "order_items" t3 ON t1."order_id" = t3."order_id"
 JOIN "reviews" t4 ON t3."isbn" = t4."isbn"
 JOIN "books" t6 ON t3."isbn" = t6."isbn"
-WHERE  (SELECT COUNT(*) FROM "books" s1 WHERE s1."author_id" = t6."author_id") > 3
+WHERE  (SELECT COUNT(*) FROM "addresses" s1 WHERE s1."city" = t2."city") > 3
 GROUP BY t2."city"
-ORDER BY 2 DESC
