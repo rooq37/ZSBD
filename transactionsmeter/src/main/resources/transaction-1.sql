0cb1362a355208119f562f22bae6dd54bd9af157
Mateusz Rokosa <mtrokosa97@gmail.com>
2020-11-14 13:25:19 +0100

    index for transaction 1

--- transactionsmeter/src/main/resources/transaction-1.sql
+++ transactionsmeter/src/main/resources/transaction-1.sql
@@ -1,4 +1,4 @@
-SELECT t1."email", (SELECT COUNT(*) FROM "orders" s1 WHERE s1."customer_email" = t1."email") ordersCount
+SELECT t1."email", (SELECT COUNT(*) FROM "orders" s1 WHERE s1."customer_email" = t1."email") ordersCount, EXTRACT( YEAR from t1."registration_date")
 FROM "customers" t1
 WHERE EXTRACT( YEAR from t1."registration_date") > 2011
-ORDER BY 2 DESC
+ORDER BY 3 DESC
