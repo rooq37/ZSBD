0534ed1e35c3bdb3fbb921d3ae2a83fa0ddf3fe6
Krzysztof Pilarczyk <radiator0@o2.pl>
2020-11-16 21:01:50 +0100

    Improved transaction-1.sql

--- transactionsmeter/src/main/resources/transaction-1.sql
+++ transactionsmeter/src/main/resources/transaction-1.sql
@@ -1,4 +1,5 @@
-SELECT t1."email", (SELECT COUNT(*) FROM "orders" s1 WHERE s1."customer_email" = t1."email") ordersCount, EXTRACT( YEAR from t1."registration_date")
+SELECT t1."email", (SELECT COUNT(*) FROM "orders" s1 WHERE s1."customer_email" = t1."email") ordersCount, EXTRACT( YEAR from t1."registration_date"),
+(SELECT COUNT(*) FROM "order_items" s1 JOIN "orders" s2 ON s1."order_id" = s2."order_id" WHERE t1."email" = s2."customer_email") itemsCount
 FROM "customers" t1
 WHERE EXTRACT( YEAR from t1."registration_date") > 2011
 ORDER BY 3 DESC
